975885050f0a6b540817ed839c270de3ddd37287
eu777 <35734219+eu777@users.noreply.github.com>
2020-05-05 17:24:52 +0200

    Replaced with init-mongo.js

--- initialization.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-use dbmongo;
-
-db.createUser({ user: "user", pwd: "password", roles: [{ role: "dbOwner", db: "dbmongo" }]});
-
-db.insertMany([{ item: "apple", price: 5 }, { item: "banana", price: 10 }]);
