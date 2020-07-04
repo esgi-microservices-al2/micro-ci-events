497ba123a65d345ac757e6b0f723aa8764455420
Joris Demandre <joris.demandre@agence-soon.fr>
2020-07-04 11:57:24 +0200

    fix consumer rabbitmq configuration

--- code/workers/MQconsumer.js
+++ code/workers/MQconsumer.js
@@ -2,8 +2,9 @@
 
 const amqp = require('amqplib');
 let rabbitmq_user_passwd = process.env.RABBITMQ_USER ? process.env.RABBITMQ_USER + ':' + process.env.RABBITMQ_PASS + "@" : "";
-let rabbitmq_url = process.env.RABBITMQ_URL || 'rabbitmq';
-let rabbitmq_connector = 'amqp://' + rabbitmq_user_passwd + rabbitmq_url;
+let rabbitmq_host = process.env.RABBITMQ_HOST || 'rabbitmq';
+let rabbitmq_port = process.env.RABBITMQ_PORT || '5672';
+let rabbitmq_connector = 'amqp://' + rabbitmq_user_passwd + rabbitmq_host + ':' + rabbitmq_port;
 const MQSaveService = require('../Services/MQSave');
 
 let import_consume = function()
