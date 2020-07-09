e732940a2fe932b9237cd6dd82d017d85a7c52c1
Joris Demandre <joris.demandre@agence-soon.fr>
2020-07-09 23:45:39 +0200

    add for history : deregister all services

--- code/services/ConsulSDService.js
+++ code/services/ConsulSDService.js
@@ -50,6 +50,31 @@ consul.agent.service.register(details, err => {
             console.log('de-registered.', err);
             process.exit();
         });
+
+        /*fix for deregistering old ones
+        [
+            "events-1ab1a2c2-943c-4d5d-b71a-2700ec2a5e1c",
+            "events-4d8e7fd2-5188-4ebd-8f27-5c4def193280",
+            "events-5b05bd9f-f353-489b-86db-7adb122a49c7",
+            "events-733e9620-4af3-4f53-84f6-e2fa47a9fc51",
+            "events-749e4304-2f08-4338-9ab2-7b35e3c8d9fd",
+            "events-7ea69372-9405-443b-8750-abd0dd6be0b7",
+            "events-85cf99df-0951-4796-9774-321e3c914a3b",
+            "events-b08f1b5a-ef30-4f2b-86ed-4e1c16d251cb",
+            "events-ca82d357-abd8-454f-890e-696ee8330c8d",
+            "events-ed06018f-f07b-471f-88cc-f7ac47fbbe94",
+            'events'
+        ].map(function(consul_id) {
+            console.log(sigName + '. De-Registering...');
+            let details = {
+                id: consul_id,
+                token: process.env.CONSUL_TOKEN || null};
+
+            consul.agent.service.deregister(details, (err) => {
+                console.log('de-registered.', err);
+                process.exit();
+            });
+        });*/
     });
 });
 
