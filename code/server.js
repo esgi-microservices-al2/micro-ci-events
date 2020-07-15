c6d08c65ad94214ef5fc4ff97268add2ff7800bd
OOualiken <oouirdia@yahoo.com>
2020-07-15 02:49:14 +0200

    add cors

--- code/server.js
+++ code/server.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const app = express();
+const cors = require('cors');
+
 const port = process.env.PORT || 8500;
 
 const MQService = require('./services/RabbitMQService');
 const DBService = require('./services/MongoDBService');
 let mqService = new MQService();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/event', require('./routes/events.router')(new DBService(), mqService));
 app.use('/build', require('./routes/build.router')(new DBService()));
